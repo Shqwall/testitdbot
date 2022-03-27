8d0cb071bffcebbeeb017a98c1e6e92657b453ad
Shqwall B <shqwall@gmail.com>
2022-03-27 06:15:59 +0300

    first commit

--- index.js
+++ index.js
@@ -1,19 +1,7 @@
 const { Telegraf } = require('telegraf')
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
-bot.start((ctx) => ctx.reply('Привет/ Это меню Бот'))
-bot.help((ctx) => ctx.reply('Send me a s55ticker'))
-bot.on('sticker', (ctx) => ctx.reply('👍'))
-bot.hears('hi', (ctx) => ctx.reply('Hey there'))
-bot.launch()
-
-// Enable graceful stop
-process.once('SIGINT', () => bot.stop('SIGINT'))
-process.once('SIGTERM', () => bot.stop('SIGTERM'))
-const { Telegraf } = require('telegraf')
-
-const bot = new Telegraf(process.env.BOT_TOKEN)
-bot.command('oldschool', (ctx) => ctx.reply('Hello'))
+bot.command('z', (ctx) => ctx.reply('Hello'))
 bot.command('hipster', Telegraf.reply('λ'))
 bot.launch()
 
