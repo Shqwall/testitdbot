f14787f78e0956b074b7e9044e3afea1cb9dff1c
Shqwall B <shqwall@gmail.com>
2022-04-14 02:35:05 +0300

    fist commit

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-const { Telegraf } = require('telegraf')
+const { Telegraf, Markup } = require('telegraf')
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
 bot.command('z', (ctx) => ctx.reply('Hello'))
 bot.command('hipster', Telegraf.reply('λ'))
 bot.command('menu', Telegraf.reply('😍'))
+bot.command('mm', (ctx) => {
+    ctx.replyWithHTML('<b>Курсы</b>', Markup.inlineKeyboard(
+        [
+            [Markup.button.callback('Пиццы', 'bnt_1'), Markup.button.callback('Комбо', 'bnt_2')]
+        ]
+    ))
 bot.launch()
 
 // Enable graceful stop
